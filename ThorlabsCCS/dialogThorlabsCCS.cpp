4b2df8e3f1eb5956216fc8df09081ceee8e566d0
Marc Gronle <gronle@ito.uni-stuttgart.de>
2015-07-03 11:41:23 +0200

    x-roi adjustable via config dialog in thorlabsCCS

--- ThorlabsCCS/dialogThorlabsCCS.cpp
+++ ThorlabsCCS/dialogThorlabsCCS.cpp
@@ -36,6 +36,9 @@ void DialogThorlabsCCS::parametersChanged(QMap<QString, ito::Param> params)
     {
         setWindowTitle(QString((params)["name"].getVal<char*>()) + " - " + tr("Configuration Dialog"));
 
+        ito::RectMeta *rm = static_cast<ito::RectMeta*>(params["roi"].getMeta());
+        ui.rangeX01->setLimitsFromIntervalMeta(rm->getWidthRangeMeta());
+
         //this is the first time that parameters are sent to this dialog,
         //therefore you can add some initialization work here
         m_firstRun = false;
@@ -43,6 +46,13 @@ void DialogThorlabsCCS::parametersChanged(QMap<QString, ito::Param> params)
         //now activate group boxes, since information is available now (at startup, information is not available, since parameters are sent by a signal)
         enableDialog(true);
     }
+
+    int *roi = params["roi"].getVal<int*>();
+    qDebug() << roi[0] << roi[1] << roi[2] << roi[3];
+    ui.rangeX01->setValues(roi[0], roi[0] + roi[2] - 1);
+    ui.rangeX01->setEnabled(! (params["roi"].getFlags() & ito::ParamBase::Readonly));
+
+    ui.spinSizeX->setValue(params["sizex"].getVal<int>());
 
     ui.sliderIntegrationTime->setMinimum(params["integration_time"].getMin()*1000);
     ui.sliderIntegrationTime->setMaximum(params["integration_time"].getMax()*1000);
@@ -57,6 +67,21 @@ ito::RetVal DialogThorlabsCCS::applyParameters()
     ito::RetVal retValue(ito::retOk);
     QVector<QSharedPointer<ito::ParamBase> > values;
     bool success = false;
+
+    if(ui.rangeX01->isEnabled())
+    {
+        int x0, x1;
+        ui.rangeX01->values(x0,x1);
+        int roi[] = {0,0,0,0};
+        memcpy(roi, m_currentParameters["roi"].getVal<int*>(), 4*sizeof(int));
+
+        if (roi[0] != x0 || roi[2] != (x1-x0+1))
+        {
+            roi[0] = x0;
+            roi[2] = x1-x0+1;
+            values.append(QSharedPointer<ito::ParamBase>(new ito::ParamBase("roi", ito::ParamBase::IntArray, 4, roi)));
+        }
+    }
 
     if(ui.sliderIntegrationTime->isEnabled())
     {
@@ -98,4 +123,19 @@ void DialogThorlabsCCS::enableDialog(bool enabled)
 {
     //e.g.
     ui.groupBoxIntegration->setEnabled(enabled);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------
+void DialogThorlabsCCS::on_rangeX01_valuesChanged(int minValue, int maxValue)
+{
+    ui.spinSizeX->setValue(maxValue - minValue + 1);
+}
+
+//------------------------------------------------------------------------------
+void DialogThorlabsCCS::on_btnFullROI_clicked()
+{
+    if (m_currentParameters.contains("sizex"))
+    {
+        ui.rangeX01->setValues(0, m_currentParameters["sizex"].getMax());
+    }
 }
