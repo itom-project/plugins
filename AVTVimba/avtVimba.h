a0ebe3659b75b3c2180d75d9c5f81e876ec4e385
Marc Gronle <gronle@ito.uni-stuttgart.de>
2016-02-10 17:21:01 +0100

    bugfix in AVTVimba, corrected copyright information

--- AVTVimba/avtVimba.h
+++ AVTVimba/avtVimba.h
@@ -1,8 +1,24 @@
 /* ********************************************************************
-    Template for a camera / grabber plugin for the software itom
-    
-    You can use this template, use it in your plugins, modify it,
-    copy it and distribute it without any license restrictions.
+Plugin "Roughness" for itom software
+URL : http ://www.uni-stuttgart.de/ito
+Copyright(C) 2016, Institut fuer Technische Optik (ITO), 
+                   Universitaet Stuttgart, Germany; 
+                   IPROM, TU Braunschweig, Germany
+
+This file is part of a plugin for the measurement software itom.
+
+This itom - plugin is free software; you can redistribute it and / or modify it
+under the terms of the GNU Library General Public Licence as published by
+the Free Software Foundation; either version 2 of the Licence, or(at
+your option) any later version.
+
+itom and its plugins are distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU Library
+General Public Licence for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with itom.If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************** */
 
 #ifndef AVTVIMBA_H
