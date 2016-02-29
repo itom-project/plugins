b0ecd83f81d4f8e8cbe1376c26367b934b458825
itobiege <itobiege@ito.uni-stuttgart.de>
2016-02-29 16:17:08 +0100

    added translation file for NanotecStepMotor

---
+++ NanotecStepMotor/translation/NanotecStepMotor_de.ts
@@ -0,0 +1,336 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>DialogNanotecStepMotor</name>
+    <message>
+        <source>NanotecStepMotor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Device:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>[unknown]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Number of Axis:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Homing (Calibration)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Asynchrone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MicroSteps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> step/unit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> units/sec</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Acceleration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Decceleration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>revert the changed configurations of this axis.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DockWidgetNanotecStepMotor</name>
+    <message>
+        <source>NanotecStepMotor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Controller name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Contoller</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Number of axis: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>10</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>COM-Port:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Step Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>no</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>move step</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>curr. pos.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>target pos.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>go</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NanotecStepMotor</name>
+    <message>
+        <source>Missing answer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The clock direction mode is currently active and the clock frequency is greater than 65 kHz. Please set the frequency less than 65 kHz.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalide command: &apos;%1&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Wrong answer: &apos;%1&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Value &apos;%1&apos; is not a number!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Input 1 is set while the controller is ready again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Position error: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zero position reached</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Controller ready</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Undervoltage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Temperature of the motor controller is outside of the specified range</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Overcurrent switch-off of the dspDrive was triggered</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Incorrect data in the EEPROM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Position error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Internal error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Driver component returned one error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name of controller</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Serial port of device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Number of axis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>internal ID of axis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>number of full steps per unit (deg or mm) of axis, 0: axis not connected [default]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>unit of axis, 0: degree [default], 1: mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>asychronous (1) or sychronous (0) [default] mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>micro steps for motor [1, 2, 4, 5, 8, 10, 16, 32, 64]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>motor shaft acceleration, range: 1..65.535 [default 2.364]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>motor shaft deceleration, range: 1..65.535 [default 2.364]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>target speed range: 1..1.000.000 [default 1.000]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>coil current if motor is running, range: 0..100</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>coil current if motor is in rest, range: 0..100</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Doesn&apos;t fit to interface DataIO!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No device given!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Number of given axisSteps is different to number of axisID!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Number of given units is different to number of axisID!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>any axis is moving. Parameters cannot be set</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Not implemented, use calibmode to set actual position as zero.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Given asis (%1) out of range!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Too much axes given!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Any motor is running. Further action is not possible!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>interrupt occurred</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NanotecStepMotorInterface</name>
+    <message>
+        <source>An initialized SerialIO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>internal ID of axis (default 1, 2, 3, ...), range: 1..254</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>number of full steps per unit (deg or mm) of axis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>unit of axis, 0: degree [default], 1: mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>NanotecStepMotor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>NanotecStepMotor is an itom-plugin, which can be used to communicate with the Nanotec controllers SMCP.
+It has been tested with one connected controller SMCP33 and 4 axis.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>licensed under LGPL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>N.A.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
