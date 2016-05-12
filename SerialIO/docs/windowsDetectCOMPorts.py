e84778e746cd0e8cbceb67713d1b6c6fecd65492
Marc Gronle <gronle@ito.uni-stuttgart.de>
2016-05-12 09:48:00 +0200

    demo script added to serialIO about detecting COM ports at Windows computers

---
+++ SerialIO/docs/windowsDetectCOMPorts.py
@@ -0,0 +1,25 @@
+'''This script can be used as example for Windows
+to detect registered COM ports for this computer'''
+import winreg as wreg
+
+def DetectCOMPorts():
+    try:
+        regconn = wreg.ConnectRegistry( None, wreg.HKEY_LOCAL_MACHINE )
+        key = wreg.OpenKey( regconn, "HARDWARE\\DEVICEMAP\\SERIALCOMM", wreg.KEY_READ )
+        values_count = wreg.QueryInfoKey( key )[1]
+        values_list = []
+        for i in range( values_count ):
+            values_list.append( wreg.EnumValue( key, i ) )
+    except ( WindowsError, EnvironmentError ):
+        print( "Unable to Connect to the Window Registry and read keys" )
+    finally:
+        key.Close()
+
+    return values_list
+
+def NumberOfCOMPorts( values_list ):
+    for subkey in iter( values_list ):
+        print( "Name : " + subkey[0] )
+        print( "Data : " + subkey[1] )
+
+NumberOfCOMPorts( DetectCOMPorts() )
