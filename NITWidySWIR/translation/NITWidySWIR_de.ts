0d7dd41dd636c53a5eb9990c492bf46964b9647c
itobiege <itobiege@ito.uni-stuttgart.de>
2017-04-07 08:54:31 +0200

    added German translation file

---
+++ NITWidySWIR/translation/NITWidySWIR_de.ts
@@ -0,0 +1,319 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>NITWidySWIR</name>
+    <message>
+        <source>Name of plugin.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Model ID of connected device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Firmware version of connectecd device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Serial number of connected device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Region of Interest ROI [x0, y0, width, height].</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Width of ROI (x-direction).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Height of ROI (y-direction).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bits per pixel bpp (8,14,32).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Integrationtime of connected device (0.1..25.6) in [s].</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Gain of camera (0..7.5) in [no unit].</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Framerate of image acquisition (0..1000) in [fps].</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pixel clock of device (12.5, 16.66, 20, 25, 33.33, 40, 50, 66.66, 80) in [MHz].</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Histogram threshold of image acquisition (0.025, 0.1, 0.4, 1.6) in [%].</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Trigger Mode of connected device (Disabled, Input, Output). Use Disabled for software trigger.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Shutter Mode of connected device (Global Shutter, Rolling).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Offset of image acquisition (0..100) in [%].</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>enables pixel correction by using NIT NUC files. By changing the integration_time the NUC file is changed, too.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Path of the NUC pixel correction files.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>One device is already connected. For a new instance, you must define a deviceNumber.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Camera ist already initialized.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>NITLibrary Exception: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>wrong Histogram value returned: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Given Trigger Mode is not defined.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Given Shutter Mode is not defined.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The grabber has already been stopped.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tried to acquire an image without having started the device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tried to get picture without triggering exposure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Empty object handle retrieved from caller</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Can not load YAML file. File does not exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Can not load YAML file. CV Exception: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NITWidySWIRInterface</name>
+    <message>
+        <source>If printManual is set to 1, the parameters of the camera are printed into the itom shelf.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If printParameterValues is set to 1, all available values of the parameters are printed into the itom shelf.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Number of the device, which you want to connect.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Plugin for cameras from NEW IMAGING TECHNOLOGIES (NIT).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>LPGL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>N.A.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dialogNITWidySWIR</name>
+    <message>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>bpp:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>shutter mode:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>trigger mode:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Integration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>gain:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>histogram threshold:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> MHz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> %</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>pixel clock:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>integration time:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>offset:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>framerate:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> fps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>x1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>x0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>y0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>y1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Configuration Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>dockWidgetNITWidySWIR</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>[unknown]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Serial#:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Model ID:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Firmware#:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Image Dimensions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>width:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>height:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>bits per pixel:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>integration time and gain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Gain:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Integration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Offset:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> %</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
