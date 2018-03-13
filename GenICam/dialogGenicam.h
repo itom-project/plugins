487519d27db98ef5b484254e6e0355470fd92b35
M. Gronle <marc.gronle@de.trumpf.com>
2018-03-13 15:49:12 +0100

    GenICam: corrected copyright text in source file

--- GenICam/dialogGenicam.h
+++ GenICam/dialogGenicam.h
@@ -1,23 +1,23 @@
 /* ********************************************************************
-    Plugin "IDSuEye" for itom software
-    URL: http://www.bitbucket.org/itom/plugins
-    Copyright (C) 2014, Pulsar Photonics GmbH, Aachen
-    Copyright (C) 2017, Institut fuer Technische Optik, Universitaet Stuttgart
+Plugin "GenICam" for itom software
+URL: http://www.uni-stuttgart.de/ito
+Copyright (C) 2018, Institut fuer Technische Optik (ITO),
+Universitaet Stuttgart, Germany
 
-    This file is part of a plugin for the measurement software itom.
-  
-    This itom-plugin is free software; you can redistribute it and/or modify it
-    under the terms of the GNU Library General Public Licence as published by
-    the Free Software Foundation; either version 2 of the Licence, or (at
-    your option) any later version.
+This file is part of a plugin for the measurement software itom.
 
-    itom and its plugins are distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library
-    General Public Licence for more details.
+This itom-plugin is free software; you can redistribute it and/or modify it
+under the terms of the GNU Library General Public Licence as published by
+the Free Software Foundation; either version 2 of the Licence, or (at
+your option) any later version.
 
-    You should have received a copy of the GNU Library General Public License
-    along with itom. If not, see <http://www.gnu.org/licenses/>.
+itom and its plugins are distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library
+General Public Licence for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with itom. If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************** */
 
 #ifndef DIALOGGENICAM_H
