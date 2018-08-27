dd72bbf5e1dec49fcd8547abeb1795a004e65dfd
robin92 <hahn@ito.uni-stuttgart.de>
2018-08-27 17:23:19 +0200

    Newport2936: added plugin

---
+++ Newport2936/dialogNewport2936.cpp
@@ -0,0 +1,162 @@
+/* ********************************************************************
+    Template for a camera / grabber plugin for the software itom
+    
+    You can use this template, use it in your plugins, modify it,
+    copy it and distribute it without any license restrictions.
+*********************************************************************** */
+
+#include "dialogNewport2936.h"
+
+#include "common/addInInterface.h"
+
+#include <qdialogbuttonbox.h>
+#include <qvector.h>
+#include <qsharedpointer.h>
+
+//----------------------------------------------------------------------------------------------------------------------------------
+DialogNewport2936::DialogNewport2936(ito::AddInBase *grabber) :
+    AbstractAddInConfigDialog(grabber),
+    m_firstRun(true),
+    m_plugin(grabber)
+{
+    ui.setupUi(this);
+
+    //disable dialog, since no parameters are known yet. Parameters will immediately be sent by the slot parametersChanged.
+    enableDialog(false);
+};
+
+
+//----------------------------------------------------------------------------------------------------------------------------------
+void DialogNewport2936::parametersChanged(QMap<QString, ito::Param> params)
+{
+    //save the currently set parameters to m_currentParameters
+    m_currentParameters = params;
+    
+    if (m_firstRun)
+    {
+        setWindowTitle(QString((params)["name"].getVal<char*>()) + " - " + tr("Configuration Dialog"));
+        ui.spinWavelengthA->setMinimum(params["wavelengthA"].getMin());
+        ui.spinWavelengthA->setMaximum(params["wavelengthA"].getMax());
+
+        if (params["channels"].getVal<int>() == 1)
+        {
+            ui.spinWavelengthB->setEnabled(false);
+            ui.comboFilterB->setEnabled(false);
+            ui.checkAttenB->setEnabled(false);
+            ui.btnZeroB->setEnabled(false);
+        }
+        else
+        {
+            ui.spinWavelengthB->setMinimum(params["wavelengthB"].getMin());
+            ui.spinWavelengthB->setMaximum(params["wavelengthB"].getMax());
+        }
+        m_firstRun = false;
+        enableDialog(true);
+    }
+    ui.comboFilterA->setCurrentIndex(params["filterTypeA"].getVal<int>());
+    ui.spinWavelengthA->setValue(params["wavelengthA"].getVal<int>());
+    ui.checkAttenA->setChecked(params["attenuatorA"].getVal<int>());
+    if (params["channels"].getVal<int>() == 1)
+    {
+        ui.comboFilterB->setCurrentIndex(params["filterTypeB"].getVal<int>());
+        ui.spinWavelengthB->setValue(params["wavelengthB"].getVal<int>());
+        ui.checkAttenB->setChecked(params["attenuatorB"].getVal<int>());
+    }
+
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------
+ito::RetVal DialogNewport2936::applyParameters()
+{
+    ito::RetVal retValue(ito::retOk);
+    QVector<QSharedPointer<ito::ParamBase> > values;
+    bool success = false;
+    if (m_currentParameters["wavelengthA"].getVal<int>() != ui.spinWavelengthA->value())
+    {
+        values.append(QSharedPointer<ito::ParamBase>(new ito::ParamBase("wavelengthA", ito::ParamBase::Int, ui.spinWavelengthA->value())));
+    }
+    if (m_currentParameters["filterTypeA"].getVal<int>() != ui.comboFilterA->currentIndex())
+    {
+        values.append(QSharedPointer<ito::ParamBase>(new ito::ParamBase("filterTypeA", ito::ParamBase::Int, ui.comboFilterA->currentIndex())));
+    }
+    if (m_currentParameters["attenuatorA"].getVal<int>() != ui.checkAttenA->isChecked())
+    {
+        if (ui.checkAttenA->isChecked())
+        {
+            values.append(QSharedPointer<ito::ParamBase>(new ito::ParamBase("attenuatorA", ito::ParamBase::Int, 1)));
+        }
+        else
+        {
+            values.append(QSharedPointer<ito::ParamBase>(new ito::ParamBase("attenuatorA", ito::ParamBase::Int, 0)));
+        }
+    }
+    if (m_currentParameters["channels"].getVal<int>() == 2)
+    {
+        if (m_currentParameters["attenuatorB"].getVal<int>() != ui.checkAttenB->isChecked())
+        {
+            values.append(QSharedPointer<ito::ParamBase>(new ito::ParamBase("attenuatorB", ito::ParamBase::Double, ui.checkAttenB->isChecked())));
+        }
+        if (m_currentParameters["filterTypeB"].getVal<int>() != ui.comboFilterB->currentIndex())
+        {
+            values.append(QSharedPointer<ito::ParamBase>(new ito::ParamBase("filterTypeB", ito::ParamBase::Double, ui.comboFilterB->currentIndex())));
+        }
+        if (m_currentParameters["wavelengthB"].getVal<int>() != ui.spinWavelengthB->value())
+        {
+            values.append(QSharedPointer<ito::ParamBase>(new ito::ParamBase("wavelengthB", ito::ParamBase::Int, ui.spinWavelengthB->value())));
+        }
+    }
+
+
+
+    retValue += setPluginParameters(values, msgLevelWarningAndError);
+
+    return retValue;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void DialogNewport2936::on_buttonBox_clicked(QAbstractButton* btn)
+{
+    ito::RetVal retValue(ito::retOk);
+
+    QDialogButtonBox::ButtonRole role = ui.buttonBox->buttonRole(btn);
+
+    if (role == QDialogButtonBox::RejectRole)
+    {
+        reject(); //close dialog with reject
+    }
+    else if (role == QDialogButtonBox::AcceptRole)
+    {
+        accept(); //AcceptRole
+    }
+    else
+    {
+        applyParameters(); //ApplyRole
+    }
+}
+//---------------------------------------------------------------------------------------------------------------------
+void DialogNewport2936::on_btnZeroA_clicked()
+{
+    ItomSharedSemaphore* waitCond = new ItomSharedSemaphore();
+    ui.group1->setEnabled(false);
+    QMetaObject::invokeMethod(m_plugin, "zeroDevice",Q_ARG(int, 1) ,Q_ARG(ItomSharedSemaphore*, waitCond));
+    observeInvocation(waitCond, msgLevelWarningAndError);
+    ui.group1->setEnabled(true);
+    waitCond->deleteSemaphore();
+    waitCond = NULL;
+}
+void DialogNewport2936::on_btnZeroB_clicked()
+{
+    ItomSharedSemaphore* waitCond = new ItomSharedSemaphore();
+    ui.group1->setEnabled(false);
+    QMetaObject::invokeMethod(m_plugin, "zeroDevice", Q_ARG(int, 2), Q_ARG(ItomSharedSemaphore*, waitCond));
+    observeInvocation(waitCond, msgLevelWarningAndError);
+    ui.group1->setEnabled(true);
+    waitCond->deleteSemaphore();
+    waitCond = NULL;
+}
+//---------------------------------------------------------------------------------------------------------------------
+void DialogNewport2936::enableDialog(bool enabled)
+{
+    //e.g.
+    ui.group1->setEnabled(enabled);
+}
---
+++ Newport2936/dockWidgetNewport2936.cpp
@@ -0,0 +1,129 @@
+/* ********************************************************************
+    Template for a camera / grabber plugin for the software itom
+    
+    You can use this template, use it in your plugins, modify it,
+    copy it and distribute it without any license restrictions.
+*********************************************************************** */
+
+#include "dockWidgetNewport2936.h"
+
+
+//----------------------------------------------------------------------------------------------------------------------------------
+DockWidgetNewport2936::DockWidgetNewport2936(ito::AddInDataIO *grabber) :
+    AbstractAddInDockWidget(grabber),
+    m_inEditing(false),
+    m_firstRun(true),
+    m_plugin(grabber),
+    m_timerIsRunning(false)
+{
+    qRegisterMetaType<QSharedPointer<QList<double> > >("QSharedPointer<QList<double> >");
+    ui.setupUi(this);
+    QPointer<ito::AddInBase> plugin(grabber);
+    ui.editorWidget->setPlugin(plugin);
+}
+void DockWidgetNewport2936::parametersChanged(QMap<QString, ito::Param> params)
+{
+
+    if (m_firstRun)
+    {
+        ui.lcdValueA->setPalette(Qt::red);
+        ui.lcdValueB->setPalette(Qt::red);
+        m_firstRun = false;
+    }
+}
+//----------------------------------------------------------------------------------------------------------------------------------
+void DockWidgetNewport2936::identifierChanged(const QString &identifier)
+{
+    ui.label_Identifier->setText(identifier);
+    ui.editorWidget->refresh();
+}
+//----------------------------------------------------------------------------------------------------------------------------------
+void DockWidgetNewport2936::on_checkAutograbbing_stateChanged(int val)
+{
+    manageTimer(true);
+}
+//----------------------------------------------------------------------------------------------------------------------------------
+void DockWidgetNewport2936::timerEvent(QTimerEvent *event)
+{
+
+    ito::RetVal retval(ito::retOk);
+    ItomSharedSemaphore* waitCond = new ItomSharedSemaphore();
+    QSharedPointer<QList<double> > value = QSharedPointer<QList <double> >(new QList<double>);
+    QPair<double, QString> result(0, "");
+    QMetaObject::invokeMethod(m_plugin, "acquireAutograbbing", Q_ARG(QSharedPointer<QList<double> >, value), Q_ARG(ItomSharedSemaphore*, waitCond));
+    if (waitCond->waitAndProcessEvents(10000))
+    {
+        retval += waitCond->returnValue;
+        if (!retval.containsError())
+        {
+            double i = value->at(0);
+            calculateUnit(value->at(0), result);
+            ui.lcdValueA->display(result.first);
+            ui.unitA->setText(result.second);
+            if (value->length() == 2)
+            {
+              calculateUnit(value->at(1), result);
+              ui.lcdValueB->display(result.first);
+              ui.unitB->setText(result.second);
+            }
+        }
+    }
+    waitCond->deleteSemaphore();
+
+
+}
+//----------------------------------------------------------------------------------------------------------------------------------
+void DockWidgetNewport2936::manageTimer(const bool &visible)
+{
+    if (visible && ui.checkAutograbbing->checkState() == Qt::Checked)
+    {
+        m_timerId = startTimer(100);
+        m_timerIsRunning = true;
+    }
+    else if (m_timerIsRunning)
+    {
+        killTimer(m_timerId);
+        m_timerIsRunning = false;
+    }
+}
+//----------------------------------------------------------------------------------------------------------------------------------
+void DockWidgetNewport2936::calculateUnit(const double &val, QPair<double, QString> &result)
+{
+
+    double exp(log10(abs(val)));
+    if (exp >= 0.0)
+    {
+        result.first = val;
+        result.second = "W";
+    }
+    else if (exp >= -3.0)
+    {
+        result.first = val*10e2;
+        result.second = "mW";
+    }
+    else if (exp >= -6.0)
+    {
+        result.first = val*10e5;
+        result.second = QString::fromLatin1("\u00B5W");
+    }
+    else if (exp >= -9.0)
+    {
+        result.first = val*10e8;
+        result.second = "nW";
+    }
+    else if (exp >= -12.0)
+    {
+        result.first = val*10e11;
+        result.second = "pW";
+    }
+    else if (exp >= -15.0)
+    {
+        result.first = val*10e14;
+        result.second = "fW";
+    }
+    else
+    {
+        result.first = val;
+        result.second = "unknown";
+    }
+}
