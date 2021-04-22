25f2969c009cbbdacac7f665b4b26360ebce1d3a
Johann <johannkrauter@googlemail.com>
2021-04-22 22:59:18 +0200

    OpenCVFilters: added getRotationMatrix2D

--- OpenCVFilters/pluginVersion.h
+++ OpenCVFilters/pluginVersion.h
@@ -1,7 +1,7 @@
 /* ********************************************************************
     itom software
     URL: http://www.uni-stuttgart.de/ito
-    Copyright (C) 2018, Institut fuer Technische Optik (ITO),
+    Copyright (C) 2021, Institut fuer Technische Optik (ITO),
     Universitaet Stuttgart, Germany
 
     This file is part of itom and its software development toolkit (SDK).
@@ -10,7 +10,7 @@
     under the terms of the GNU Library General Public Licence as published by
     the Free Software Foundation; either version 2 of the Licence, or (at
     your option) any later version.
-   
+
     In addition, as a special exception, the Institut fuer Technische
     Optik (ITO) gives you certain additional rights.
     These rights are described in the ITO LGPL Exception version 1.0,
@@ -31,14 +31,16 @@
 #include "itom_sdk.h"
 
 #define PLUGIN_VERSION_MAJOR 1
-#define PLUGIN_VERSION_MINOR 2
+#define PLUGIN_VERSION_MINOR 3
 #define PLUGIN_VERSION_PATCH 0
 #define PLUGIN_VERSION_REVISION 0
-#define PLUGIN_VERSION        CREATE_VERSION(PLUGIN_VERSION_MAJOR,PLUGIN_VERSION_MINOR,PLUGIN_VERSION_PATCH)
-#define PLUGIN_VERSION_STRING CREATE_VERSION_STRING(PLUGIN_VERSION_MAJOR,PLUGIN_VERSION_MINOR,PLUGIN_VERSION_PATCH)
-#define PLUGIN_COMPANY        "Institut fuer Technische Optik, University Stuttgart"
-#define PLUGIN_COPYRIGHT      "(C) 2016, ITO, University Stuttgart"
-#define PLUGIN_NAME           "OpenCVFilters"
+#define PLUGIN_VERSION                                                                             \
+    CREATE_VERSION(PLUGIN_VERSION_MAJOR, PLUGIN_VERSION_MINOR, PLUGIN_VERSION_PATCH)
+#define PLUGIN_VERSION_STRING                                                                      \
+    CREATE_VERSION_STRING(PLUGIN_VERSION_MAJOR, PLUGIN_VERSION_MINOR, PLUGIN_VERSION_PATCH)
+#define PLUGIN_COMPANY "Institut fuer Technische Optik, University Stuttgart"
+#define PLUGIN_COPYRIGHT "(C) 2021, ITO, University Stuttgart"
+#define PLUGIN_NAME "OpenCVFilters"
 
 //----------------------------------------------------------------------------------------------------------------------------------
 
