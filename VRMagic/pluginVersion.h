3171d1bdac9f5a49feeab242d0c46e43a34a4126
photoniker <johannkrauter@googlemail.com>
2024-04-29 13:59:22 +0200

    replaced some �

--- VRMagic/pluginVersion.h
+++ VRMagic/pluginVersion.h
@@ -36,7 +36,7 @@
 #define PLUGIN_VERSION_REVISION 0
 #define PLUGIN_VERSION        CREATE_VERSION(PLUGIN_VERSION_MAJOR,PLUGIN_VERSION_MINOR,PLUGIN_VERSION_PATCH)
 #define PLUGIN_VERSION_STRING CREATE_VERSION_STRING(PLUGIN_VERSION_MAJOR,PLUGIN_VERSION_MINOR,PLUGIN_VERSION_PATCH)
-#define PLUGIN_COMPANY        "Institut f�r Technische Optik, University Stuttgart"
+#define PLUGIN_COMPANY        "Institut für Technische Optik, University Stuttgart"
 #define PLUGIN_COPYRIGHT      "(C) 2015, ITO, University Stuttgart"
 #define PLUGIN_NAME           "VRMagic"
 
